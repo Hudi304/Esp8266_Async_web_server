4bb4ef42e314edafa266729c86d774add6e81f36
Hudi304 <hudisteanum@yahoo.com>
2022-06-25 19:30:19 +0300

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -76,26 +76,26 @@ void handleWebSocketMessage(void* arg, uint8_t* data, size_t len, AsyncWebSocket
 
 void onEvent(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type,
   void* arg, uint8_t* data, size_t len) {
-  Serial.printf("Msg Client [%u]  \n", client->id());
+  // Serial.printf("Msg Client [%u]  \n", client->id());
 
   switch (type) {
   case WS_EVT_CONNECT:
-    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    // Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
     // client->ping();
     // ws.textAll("connected");
     break;
   case WS_EVT_DISCONNECT:
-    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    // Serial.printf("WebSocket client #%u disconnected\n", client->id());
     break;
   case WS_EVT_DATA:
     // Serial.printf("message\n");
     handleWebSocketMessage(arg, data, len, client);
     break;
   case WS_EVT_PONG:
-    Serial.printf("pong\n");
+    // Serial.printf("pong\n");
     break;
   case WS_EVT_ERROR:
-    Serial.printf("error\n");
+    // Serial.printf("error\n");
     break;
   }
 }
@@ -106,7 +106,7 @@ void initWebSocket() {
 }
 
 String processor(const String& var) {
-  Serial.println(var);
+  // Serial.println(var);
   if (var == "STATE") {
     if (ledState) {
       return "ON";
@@ -125,81 +125,36 @@ const char* ap_password = "embedded-robotics"; //Access Point Password
 uint8_t max_connections = 8;//Maximum Connection Limit for AP
 int current_stations = 0, new_stations = 0;
 
-void setUpSoftAP() {
-  if (WiFi.softAP(ap_ssid, ap_password, 1, false, max_connections) == true)
-  {
-    Serial.print("Access Point is Creadted with SSID: ");
-    Serial.println(ap_ssid);
-    Serial.print("Max Connections Allowed: ");
-    Serial.println(max_connections);
-    Serial.print("Access Point IP: ");
-    Serial.println(WiFi.softAPIP());
-  }
-  else
-  {
-    Serial.println("Unable to Create Access Point");
-  }
-}
-
 void setUpStation() {
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
+    // Serial.println("Connecting to WiFi..");
   }
 }
 
 void setup() {
-  // Serial port for debugging purposes
   Serial.begin(115200);
+
+  delay(500);
+
+  Serial.swap();
+
+  delay(500);
 
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-
-  // Connect to Wi-Fi
-  // WiFi.begin(ssid, password);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(1000);
-  //   Serial.println("Connecting to WiFi..");
-  // }
-
   setUpStation();
-
   delay(2000);
-
-
-  // setUpSoftAP();
-
-  // Print ESP Local IP Address
-  Serial.println(WiFi.localIP());
+  // Serial.println(WiFi.localIP());
   initWebSocket();
   server.begin();
 }
 
-
-int secs = 0;
-
 void loop() {
-
   ws.cleanupClients();
   digitalWrite(ledPin, ledState);
 
-
-
-  // new_stations=WiFi.softAPgetStationNum();
-
-  // if(current_stations<new_stations)//Device is Connected
-  // {
-  //   current_stations=new_stations;
-  //   Serial.print("New Device Connected to SoftAP... Total Connections: ");
-  //   Serial.println(current_stations);
-  // }
-
-  // if(current_stations>new_stations)//Device is Disconnected
-  // {
-  //   current_stations=new_stations;
-  //   Serial.print("Device disconnected from SoftAP... Total Connections: ");
-  //   Serial.println(current_stations);
-  // }
+  
 }
